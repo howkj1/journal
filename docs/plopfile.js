5984ac8fa240ecf4cd71e264022fde8a4a3abb8b
Jim Howk <howkj1@gmail.com>
2018-01-04 13:58:41 -0600

    gitbook updates

--- docs/plopfile.js
+++ docs/plopfile.js
@@ -2,24 +2,24 @@ module.exports = function (plop) {
     plop.setGenerator('gitbook', {
         description: 'gitbook is awesome',
         prompts: [
-            {
-            type: 'input',
-            name: 'link',
-            message: 'paste link to online stuff or type summary; enter to continue: \n'
-        },
-            {
+          {
             type: 'input',
             name: 'name',
             message: 'Your new article name: \n',
             validate: function (value) {
-                if ((/.+/).test(value)) { return true; }
-                return 'name is required';
+              if ((/.+/).test(value)) { return true; }
+              return 'name is required';
             }
+          },
+            {
+            type: 'input',
+            name: 'link',
+            message: 'paste url or type a summary; enter to continue: \n'
         },
             {
             type: 'confirm',
             name: 'draft',
-            message: 'Is this still a Draft?'
+            message: 'Is this a Draft?'
         }],
         actions: function(data) {
             var actions = [];
